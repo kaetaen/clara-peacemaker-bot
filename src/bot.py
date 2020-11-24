017ca929cdf7f7e143def42c0f785d80099d89c0
kaetaen <kaetaen@tutanota.com>
2020-11-24 12:22:53 -0300

    Add method [brat] and method [welcome]

--- src/bot.py
+++ src/bot.py
@@ -4,4 +4,17 @@ import env
 bot = telebot.TeleBot(env.TOKEN)
 bot.remove_webhook()
 
+@bot.message_handler(commands=['start'])
+def welcome(msg):
+    bot.send_message(msg.chat.id, 'Olá, sou a Clara, posso te ajudar nos momentos de ansiedade')
+
+
+@bot.message_handler(commands=['respire'])
+def breathe(msg):
+    img = open('./assets/breath.gif', 'rb')
+    bot.send_document(msg.chat.id, img)
+
+
+if __name__ == '__main__':
+    bot.polling()
 
