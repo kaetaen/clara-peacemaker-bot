89dcb10b7109bca4afd5af638c8780cd1f72556a
kaetaen <kaetaen@tutanota.com>
2020-11-27 12:53:07 -0300

    Refactoring

--- src/utils/randomize.py
+++ src/utils/randomize.py
@@ -1,7 +1,7 @@
 import quantumrandom
 
-def get_random_file(content_name: str, items: dict):
-    content = items[content_name]
-    index = int(quantumrandom.randint(0, 2))
+def get_random_file(content_name: str, items: dict) -> str:
+    content: list = items[content_name]
+    index = int(quantumrandom.randint(0, len(content) - 1))
     
     return content[index]
