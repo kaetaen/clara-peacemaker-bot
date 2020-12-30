f158dbcf8d197f865f45c4b056954833f2cde000
kaetaen <kaetaen@tutanota.com>
2020-12-30 01:09:02 -0300

    Remove old codebase

--- src/utils/randomize.py
+++
@@ -1,7 +0,0 @@
-import quantumrandom
-
-def get_random_file(content_name: str, items: dict) -> str:
-    content: list = items[content_name]
-    index = int(quantumrandom.randint(0, len(content) - 1))
-    
-    return content[index]
