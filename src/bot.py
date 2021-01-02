e5993df345c759f3c92cace3f40ae17dfb208eee
kaetaen <kaetaen@tutanota.com>
2021-01-02 02:41:16 -0300

    Now, that's beauty refactoring! Created message module!

--- src/bot.py
+++ src/bot.py
@@ -1,13 +1,15 @@
 import telebot
 import env
 from positive_vibes_api import PositiveVibesAPI as Media
+import messages
+# Why 'Media'? Is short. Low code XD.
 
 bot = telebot.TeleBot(env.TOKEN)
 bot.remove_webhook()
 
 @bot.message_handler(commands=['start', 'sobre'])
 def welcome(msg):
-    bot.send_message(msg.chat.id, 'Olá  sou a Clara, posso te ajudar nos momentos de crise de ansiedade.  \nPara saber mais sobre o conteúdo disponibilizado acesse: positive-vibes-api.herokuapp.com/')
+    bot.send_message(msg.chat.id, messages.ABOUT)
 
 
 @bot.message_handler(commands=['respire'])
@@ -24,8 +26,7 @@ def think(msg):
 @bot.message_handler(commands=['escute'])
 def listen(msg):
     music = Media().get_music()
-    msg_title = f'🎵 Canção: {music["song_name"]} \n👨 Artista: {music["artist_name"]}'
-    bot.send_document(msg.chat.id, music["file"], caption=msg_title)
+    bot.send_document(msg.chat.id, music["file"], caption=messages.SONG_TAG(music))
 
 
 @bot.message_handler(commands=['contemple'])
@@ -37,42 +38,11 @@ def enjoy(msg):
 
 @bot.message_handler(commands=['converse'])
 def talk(msg):
-    content = """
-As vezes buscar a paz por meio da arte não é o suficiente...
-As vezes precisamos mesmo conversar com alguém.
-As vezes precisamos de alguém para nos ouvir.
-
-Não tenha medo.
-Não tenha vergonha.
-Não hesite.
-
-
-📌 CVV
-
-O Centro de Valorização da Vida realiza apoio emocional e prevenção do suicídio
-
-📞 Telefone: 188
-📧 Via email: https://www.cvv.org.br/e-mail/
-💬 Chat online: https://www.cvv.org.br/chat/
-
-
-📌 CRAS
-
-O CRAS é um local público onde são oferecidos os serviços de Assistência Social.
-Se você se encontra em situação vulnerável, é um direito seu!
-
-🔗 Conheça: https://www.gov.br/pt-br/servicos/acessar-o-cras-centro-de-referencia-da-assistencia-social
-📍 Encontre: https://aplicacoes.mds.gov.br/sagi/mops/serv-cras.php?
-
-
-📌 CAPS
-
-Os Centros de Atenção Psicossocial (CAPS) são pontos de serviços de saúde de caráter aberto e comunitário constituído por equipe multiprofissional e que atua sobre a ótica interdisciplinar e realiza prioritariamente atendimento às pessoas com sofrimento ou transtorno mental, incluindo aquelas com necessidades decorrentes do uso de álcool e outras drogas.
-
-🔗 Conheça: https://www.gov.br/saude/pt-br/acesso-a-informacao/acoes-e-programas/centro-de-atencao-psicossocial-caps
-📍 Encontre: https://sage.saude.gov.br/paineis/planoCrack/lista_caps.php?output=html&
-"""
-    bot.send_message(msg.chat.id, content, disable_web_page_preview=True)
+   bot.send_message(
+       msg.chat.id,
+       messages.EXTERNAL_REFS,
+       disable_web_page_preview=True
+   )
 
 if __name__ == '__main__':
     bot.polling()
---
+++ src/messages.py
@@ -0,0 +1,42 @@
+ABOUT = '''bot.send_message(msg.chat.id, 'Olá  sou a Clara, posso te ajudar nos momentos de crise de ansiedade.
+Para saber mais sobre o conteúdo disponibilizado acesse: positive-vibes-api.herokuapp.com/')
+'''
+
+
+SONG_TAG = lambda music:  f'🎵 Canção: {music["song_name"]} \n👨 Artista: {music["artist_name"]}'
+
+EXTERNAL_REFS = '''
+As vezes buscar a paz por meio da arte não é o suficiente...
+As vezes precisamos mesmo conversar com alguém.
+As vezes precisamos de alguém para nos ouvir.
+
+Não tenha medo.
+Não tenha vergonha.
+Não hesite.
+
+
+📌 CVV
+
+O Centro de Valorização da Vida realiza apoio emocional e prevenção do suicídio
+
+📞 Telefone: 188
+📧 Via email: https://www.cvv.org.br/e-mail/
+💬 Chat online: https://www.cvv.org.br/chat/
+
+
+📌 CRAS
+
+O CRAS é um local público onde são oferecidos os serviços de Assistência Social.
+Se você se encontra em situação vulnerável, é um direito seu!
+
+🔗 Conheça: https://www.gov.br/pt-br/servicos/acessar-o-cras-centro-de-referencia-da-assistencia-social
+📍 Encontre: https://aplicacoes.mds.gov.br/sagi/mops/serv-cras.php?
+
+
+📌 CAPS
+
+Os Centros de Atenção Psicossocial (CAPS) são pontos de serviços de saúde de caráter aberto e comunitário constituído por equipe multiprofissional e que atua sobre a ótica interdisciplinar e realiza prioritariamente atendimento às pessoas com sofrimento ou transtorno mental, incluindo aquelas com necessidades decorrentes do uso de álcool e outras drogas.
+
+🔗 Conheça: https://www.gov.br/saude/pt-br/acesso-a-informacao/acoes-e-programas/centro-de-atencao-psicossocial-caps
+📍 Encontre: https://sage.saude.gov.br/paineis/planoCrack/lista_caps.php?output=html&
+'''
